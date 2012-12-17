01ac8f872d91ccfd78b764e076d98d9931ff34cd
Or Cohen <orc@fewbytes.com>
2012-12-17 13:20:33 +0200

    Fixed usage of node instead of other_node

--- libraries/node.rb
+++ libraries/node.rb
@@ -4,7 +4,7 @@ class Chef
       # for EC2, check if nodes are on the same region
       if self["cloud"]["provider"] == "ec2"
         if self["ec2"]["placement_availability_zone"][/([a-z]{2}-[a-z]+-[0-9])[a-z]/,1] == \
-            node["ec2"]["placement_availability_zone"][/([a-z]{2}-[a-z]+-[0-9])[a-z]/,1]
+            other_node["ec2"]["placement_availability_zone"][/([a-z]{2}-[a-z]+-[0-9])[a-z]/,1]
           self["cloud"]["local_ipv4"] 
         else
           self["cloud"]["public_ipv4"]
