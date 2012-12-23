5e2afafc077170633822b4b239084dc5819eb82c
Yoni Yalovitsky <yoni@fewbytes.com>
2012-12-23 18:11:52 +0200

    minor refactoring

--- libraries/node.rb
+++ libraries/node.rb
@@ -1,41 +1,39 @@
-class Chef
-  class Node
-    def cloud_location(n)
-      case n["cloud"]
-        when "ec2"  #compare regions
-          n["ec2"]["placement_availability_zone"][/([a-z]{2}-[a-z]+-[0-9])[a-z]/,1]
-        #when adding new multi-region cloud providers - add cases here
-        when nil
-          false
-        else
-          n["cloud"]["provider"] #various single-region providers
-      end
-    end
-
-    def relative_hostname(other_node=node)
-      if not cloud_location(self)
-        self["hostname"]
-      elsif cloud_location(self) == cloud_location(other_node)
-        self["cloud"]["local_hostname"]
+class Chef::Node
+  def cloud_location(n)
+    case n["cloud"]
+      when "ec2"  #compare regions
+        n["ec2"]["placement_availability_zone"][/([a-z]{2}-[a-z]+-[0-9])[a-z]/,1]
+      #when adding new multi-region cloud providers - add cases here
+      when nil
+        false
       else
-        self["cloud"]["public_hostname"]
-      end
+        n["cloud"]["provider"] #various single-region providers
     end
+  end
 
-    def relative_ipv4(other_node=node)
-      if not cloud_location(self)
-        self["ipaddress"]
-      elsif cloud_location(self) == cloud_location(other_node)
-        self["cloud"]["local_ipv4"]
-      else
-        self["cloud"]["public_ipv4"]
-      end
+  def relative_hostname(other_node=node)
+    if not cloud_location(self)
+      self["hostname"]
+    elsif cloud_location(self) == cloud_location(other_node)
+      self["cloud"]["local_hostname"]
+    else
+      self["cloud"]["public_hostname"]
     end
+  end
 
-    def ip_for_node(other_node)
-      ::Fewbytes::Chef::Utils.deprecation_warning("ip_for_node", "relative_ipv4")
-      relative_ipv4(other_node)
+  def relative_ipv4(other_node=node)
+    if not cloud_location(self)
+      self["ipaddress"]
+    elsif cloud_location(self) == cloud_location(other_node)
+      self["cloud"]["local_ipv4"]
+    else
+      self["cloud"]["public_ipv4"]
     end
+  end
+
+  def ip_for_node(other_node)
+    ::Fewbytes::Chef::Utils.deprecation_warning("ip_for_node", "relative_ipv4")
+    relative_ipv4(other_node)
   end
 end
 
--- libraries/utils.rb
+++ libraries/utils.rb
@@ -1,3 +1,9 @@
+# Copyright: Fewbytes Technologies LTD. 2011, 2012
+# License: Apache2
+
+module Fewbytes; end #create if missing
+module Fewbytes::Chef; end #create if missing
+
 module Fewbytes::Chef::Utils
   module_function
   def deprecation_warning(replacement, deprecated=caller[0][/`([^']*)'/, 1])
